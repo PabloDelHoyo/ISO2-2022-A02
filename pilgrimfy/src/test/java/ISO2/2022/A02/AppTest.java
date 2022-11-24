2fbc73491ea3630a25dd5d104ec21eebcb33db51
Alberto Díaz <alberto.diaz17@alu.uclm.es>
2022-11-24 23:39:28 +0100

    Create Maven project

---
+++ pilgrimfy/src/test/java/ISO2/2022/A02/AppTest.java
@@ -0,0 +1,20 @@
+package ISO2.2022.A02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
