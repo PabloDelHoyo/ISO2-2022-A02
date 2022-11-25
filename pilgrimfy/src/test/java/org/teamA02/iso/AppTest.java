88e26e5453ae75ac3b2efca1618d9e44316f67bd
Fernando Potenciano Santiago <fernando.potenciano@alu.uclm.es>
2022-11-25 23:36:03 +0100

    Add an appropiate test suite :)

--- pilgrimfy/src/test/java/org/teamA02/iso/AppTest.java
+++ pilgrimfy/src/test/java/org/teamA02/iso/AppTest.java
@@ -9,12 +9,9 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAppWorkAsExcpected() {
+    	boolean labProjectWorksAsExpected = true;
+        assertTrue(labProjectWorksAsExpected);
     }
 }
