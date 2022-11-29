fcb4351d0e0476145d5a0023b7798639f1222998
Alberto Díaz <alberto.diaz17@alu.uclm.es>
2022-11-29 19:27:42 +0100

    Fix bugs detected by the test cases

--- pilgrimfy/src/main/java/org/teamA02/iso/server/BlogPostDAO.java
+++ pilgrimfy/src/main/java/org/teamA02/iso/server/BlogPostDAO.java
@@ -6,25 +6,21 @@ public class BlogPostDAO implements DAO<BlogPost>{
 
 	@Override
 	public BlogPost read(String id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean delete(BlogPost entity) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean update(BlogPost entity) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean create(BlogPost entity) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
