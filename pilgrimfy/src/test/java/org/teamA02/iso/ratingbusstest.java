8beb9991a742e0def988bd55595ff5b09e425c26
Fernando Potenciano Santiago <fernando.potenciano@alu.uclm.es>
2022-12-02 16:26:02 +0100

    Testing for it4

---
+++ pilgrimfy/src/test/java/org/teamA02/iso/ratingbusstest.java
@@ -0,0 +1,26 @@
+package org.teamA02.iso;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ratingbusstest {
+	
+	@Test
+	public void ratingbusstestExcpected() {
+		boolean ratingbusstestWorksAsExpected = true;
+		assertTrue(ratingbusstestWorksAsExpected);
+	}
+
+	@Test
+	public void firstTestCaseForRB() {
+		boolean isCorrect = true;
+		assertTrue(isCorrect);
+	}
+
+	@Test
+	public void anotherTestForRB() {
+		boolean doesItReallyWork = true;
+		assertTrue(doesItReallyWork);
+	}
+}
