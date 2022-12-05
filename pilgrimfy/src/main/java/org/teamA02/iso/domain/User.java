074f4019f40fc9d2f1af7d445d0b499faad15bba
Alberto Díaz <alberto.diaz17@alu.uclm.es>
2022-12-05 11:46:57 +0100

    Implementation for the it5

---
+++ pilgrimfy/src/main/java/org/teamA02/iso/domain/User.java
@@ -0,0 +1,40 @@
+package org.teamA02.iso.domain;
+
+public class User {
+	private String userName;
+	private String emailAddress;
+	private String password;
+	private String userRole;
+	public User(String userName, String emailAddress, String password, String userRole) {
+		super();
+		this.userName = userName;
+		this.emailAddress = emailAddress;
+		this.password = password;
+		this.userRole = userRole;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
+}
