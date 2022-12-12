4e1642134ef957c6a7adfbf3d45bc2fc285680d0
Fernando Potenciano Santiago <fernando.potenciano@alu.uclm.es>
2022-12-12 18:42:12 +0100

    Testing for it6

---
+++ pilgrimfy/src/test/java/org/teamA02/iso/CreateBlogTest.java
@@ -0,0 +1,17 @@
+
+package org.teamA02.iso;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CreateBlogTest {
+
+	@Test
+	public void testCreateBlogTest() {
+		boolean testCreateBlogTest = true;
+		assertTrue(testCreateBlogTest);
+	}
+
+
+}
