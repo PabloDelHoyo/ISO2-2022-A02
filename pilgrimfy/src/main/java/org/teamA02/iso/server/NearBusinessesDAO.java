a6e0d63c9801cfa6621a89985906991c71f08226
Fernando Potenciano Santiago <fernando.potenciano@alu.uclm.es>
2022-12-16 19:40:44 +0100

    Testing for it8 and Happy Holidays

---
+++ pilgrimfy/src/main/java/org/teamA02/iso/server/NearBusinessesDAO.java
@@ -0,0 +1,29 @@
+package org.teamA02.iso.server;
+
+import org.teamA02.iso.domain.NearBusinesses;
+
+public class NearBusinessesDAO implements DAO<NearBusinesses> {
+	@Override
+	public NearBusinesses read(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(NearBusinesses entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(NearBusinesses entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean create(NearBusinesses entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
